fe220474d6880b0ae9e3b4a4b1499f988b417d1f
minhut <uongminh96@gmail.com>
2022-10-07 15:28:17 +0200

    update CONSTANST value for max, min quality

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -2,6 +2,8 @@ package com.gildedrose;
 
 class GildedRose {
     Item[] items;
+    private static Integer MAX_QUALITY = 50;
+    private static Integer MIN_QUALITY = 0;
 
     public GildedRose(Item[] items) {
         this.items = items;
@@ -34,14 +36,14 @@ class GildedRose {
         if (item.sellIn < 6) {
             increaseItemQualityBy1(item);
         }
-        item.sellIn = item.sellIn - 1;
+        decreaseSellInBy1(item);
         if (item.sellIn < 0) {
             item.quality = 0;
         }
     }
 
     public void updateAgedBrieItem(Item item) {
-        item.sellIn = item.sellIn - 1;
+        decreaseSellInBy1(item);
         increaseItemQualityBy1(item);
         if (item.sellIn < 0) {
             increaseItemQualityBy1(item);
@@ -49,7 +51,7 @@ class GildedRose {
     }
 
     public void updateNormalItem(Item item) {
-        item.sellIn = item.sellIn - 1;
+        decreaseSellInBy1(item);
         decreaseItemQualityBy1(item);
         if (item.sellIn < 0) {
             decreaseItemQualityBy1(item);
@@ -60,15 +62,20 @@ class GildedRose {
         //nothing to change
     }
 
+    public void decreaseSellInBy1(Item item) {
+        item.sellIn -= 1;
+    }
+
     private void increaseItemQualityBy1(Item item) {
-        if (item.quality < 50) {
+        if (item.quality < MAX_QUALITY) {
             item.quality = item.quality + 1;
         }
     }
 
     private void decreaseItemQualityBy1(Item item) {
-        if (item.quality > 0) {
+        if (item.quality > MIN_QUALITY) {
             item.quality = item.quality - 1;
         }
     }
+
 }
